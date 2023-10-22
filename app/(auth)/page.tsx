3dd15f3f54a4762b367e23f006565a31be6ccc39
Michael Slocum <mslocum819@gmail.com>
2023-10-21 20:34:29 -0700

    Add redirect to dashboard if user is logged in

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
+import { getCurrentUser } from "@/lib/session"
 import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
 import { UserAuthForm } from "@/components/user-auth-form"
@@ -10,7 +12,13 @@ export const metadata: Metadata = {
   description: "Login to your account",
 }
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await getCurrentUser()
+
+  if (user) {
+    return redirect("/overview/dashboard")
+  }
+
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
