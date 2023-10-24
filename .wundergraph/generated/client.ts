68ca6e5881ea418e9dc976294103558519456ad6
Michael Slocum <mslocum819@gmail.com>
2023-10-24 15:29:56 -0700

    Update applicationHash in defaultClientConfig

--- .wundergraph/generated/client.ts
+++ .wundergraph/generated/client.ts
@@ -1146,7 +1146,7 @@ export const WUNDERGRAPH_S3_ENABLED = false;
 export const WUNDERGRAPH_AUTH_ENABLED = false;
 
 export const defaultClientConfig: ClientConfig = {
-	applicationHash: "ab83b3fb",
+	applicationHash: "bfc869cb",
 	baseURL: "https://d2vp238439bu7d.cloudfront.net",
 	sdkVersion: "0.179.3",
 };
